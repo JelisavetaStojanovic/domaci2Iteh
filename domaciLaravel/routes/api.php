96d0c1f38a8ffc4d468ade4d33a7e43fcf9dbc8c
JelisavetaStojanovic <jelisaveta.stojanovic.00@gmail.com>
2023-02-10 20:39:11 +0100

    API routes

--- domaciLaravel/routes/api.php
+++ domaciLaravel/routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+    /* Route::get('user', [ClientController::class, 'index']);
+    Route::post('user', [ClientController::class, 'store']);
+    Route::get('user', [ClientController::class, 'show']);
+    Route::put('client', [ClientController::class, 'update']);
+    Route::delete('client', [ClientController::class, 'delete']);*/
+    Route::resource('user.client', ClientController::class)->only('index', 'show', 'store', 'update', 'destroy');
+    Route::resource('city', CityController::class)->only('index', 'show');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
